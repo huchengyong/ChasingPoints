06d0bb8f83e95e36f38074562a64d57937a4d573
Jericho <jerichohu@gmail.com>
2026-09-24 21:25:03 +0800

    ios 项目初始化后

---
+++ iOS/iOS/iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp.swift
+//  iOS
+//
+//  Created by WiseSearch on 2026/9/22.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
